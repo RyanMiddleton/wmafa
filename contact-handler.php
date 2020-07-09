6b2f5b2441f2fd1e4661eb088d926e17e1572039
Ryan Middleton <middletonry@gmail.com>
2020-07-09 12:45:19 -0700

    Added files for basic website
    Files for navigation of the site and a php file to send contact emails were added to the repository.

---
+++ contact-handler.php
@@ -0,0 +1,37 @@
+<?php
+
+$errors="";
+
+$myemail = 'middletonry@gmail.com';
+
+if(empty($_POSt['name']||empty(($_POST['email']||empty($_POST['message'])){
+    $errors.="\nError: all field are required";
+}
+
+$name = $_POSt['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+if(!preg_match(“/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i”, $email_address)){
+    $errors.="\nError:Invalid email address"
+}
+
+if(empty($errors)){
+    $to = $myemail;
+
+    $email_subject = “Contact form submission: $name”;
+
+    $email_body = “You have received a new message. “.
+
+    ” Here are the details:\n Name: $name \n “.
+
+    “Email: $email_address\n Message \n $message”;
+
+    $headers = “From: $myemail\n”;
+
+    $headers .= “Reply-To: $email_address”;
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header(‘Location: contact-thank-you.html’);
+}
+?>
